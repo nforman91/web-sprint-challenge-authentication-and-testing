d22ab76003a9f92a748d6a4af0d9cdb15503133e
Nathan <nathan.forman@gmail.com>
2021-11-12 13:11:00 -0800

    Added endpoints to server

--- api/server.js
+++ api/server.js
@@ -16,6 +16,42 @@ server.use(express.json());
 server.use('/api/auth', authRouter);
 server.use('/api/jokes', restrict, jokesRouter); // only logged-in users should have access!
 
+const Users = require('./jokes/jokes-model')
+
+// server.get("/", (req, res) => {
+//   res.status(200).json({ api: "up" });
+// });
+
+// server.get("/users", (req, res) => {
+//   Users.getAll()
+//     .then(users => {
+//       res.status(200).json(users);
+//     })
+//     .catch(error => {
+//       res.status(500).json(error);
+//     });
+// });
+
+server.get("/users/:id", (req, res) => {
+  Users.findById(req.params.id)
+    .then(user => {
+      res.status(200).json(user)
+    })
+    .catch(err => {
+      res.status(500).json(err)
+    })
+});
+
+server.post("/users", async (req, res) => {
+  Users.add(req.body)
+    .then(user => {
+      res.status(201).json(user)
+    })
+    .catch(err => {
+      res.status(500).json(err)
+    })
+});
+
 server.use((err, req, res, next) => { // eslint-disable-line
   res.status(err.status || 500).json({
     message: err.message,
