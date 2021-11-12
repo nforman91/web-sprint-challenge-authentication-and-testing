ba6ffc8de4a786ba69c8b98b683da9e62a956fba
Nathan <nathan.forman@gmail.com>
2021-11-12 11:49:31 -0800

    Added error handling middleware to server

--- api/server.js
+++ api/server.js
@@ -16,4 +16,15 @@ server.use(express.json());
 server.use('/api/auth', authRouter);
 server.use('/api/jokes', restrict, jokesRouter); // only logged-in users should have access!
 
+// server.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client', 'index.html'));
+// });
+
+server.use((err, req, res, next) => { // eslint-disable-line
+  res.status(err.status || 500).json({
+    message: err.message,
+    stack: err.stack,
+  });
+});
+
 module.exports = server;
